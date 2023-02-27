41b1d44a16376c9675396e328d78e9824f37a934
diagon <soumyardhal03@gmail.com>
2023-02-27 15:07:49 +0530

    made changes to convert

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Navigationbar from './Navigationbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { TextareaAutosize } from '@mui/material';
-import { Button } from '@mui/material';
 import Heading from './Heading';
 import Description from './Description';
 
 function HomePage() {
     return (
       <div className="App">
+        <div className='bg1'></div>
         <div className='homepage-container'>
           <Heading/>
           <Description/>
--- src/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-<TextField
-          variant='outlined'
-          placeholder='Input the Scripts'
-          multiline
-          rows={10}
-          maxRows={20}
-        />
